dd16cfbb69ce1b62c7cd5ea2bcc51a156e4ef722
Administrator <11360847@qq.com>
2026-03-21 17:07:32 +0800

    init

---
+++ Framework/CaiXin.EntityFrameworkCore/CaiXinDbContextFactory.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaiXin.EntityFrameworkCore
+{
+    public class CaiXinDbContextFactory : IDesignTimeDbContextFactory<CaiXinContext>
+    {
+        public CaiXinContext CreateDbContext(string[] args)
+        {
+            var configuration = BuildConfguration();
+
+            var builder = new DbContextOptionsBuilder<CaiXinContext>()
+                .UseSqlServer(
+                configuration.GetConnectionString("ConnectionString"),
+                optionsBuilder => optionsBuilder.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName)
+                );
+            return new CaiXinContext(builder.Options);
+        }
+
+        private static IConfigurationRoot BuildConfguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appseting.json", false);
+            return builder.Build();
+        }
+    }
+}
