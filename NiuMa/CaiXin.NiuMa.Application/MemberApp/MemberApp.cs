8e52be8b88cf497ba6ae1d33b5afaa1943fc467b
caxin <11360847@qq.com>
2026-03-23 10:00:01 +0800

    init

--- NiuMa/CaiXin.NiuMa.Application/MemberApp/MemberApp.cs
+++ NiuMa/CaiXin.NiuMa.Application/MemberApp/MemberApp.cs
@@ -4,6 +4,8 @@ using CaiXin.NiuMa.Application.Contracts.MemberApp.Eto;
 using CaiXin.NiuMa.Domain.Member;
 using CaiXin.NiuMa.Domain.Shared.Response;
 using Mapster;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Volo.Abp.Application.Services;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.Domain.Repositories;
@@ -17,48 +19,46 @@ namespace CaiXin.NiuMa.Application.MemberApp
     {
         private ILocalEventBus LocalEventBus => LazyServiceProvider.LazyGetRequiredService<ILocalEventBus>();
 
-        private IRepository<User, Guid> UserRepo => LazyServiceProvider.LazyGetRequiredService<IRepository<User, Guid>>();
 
         private IUserRepository UserRepository => LazyServiceProvider.LazyGetRequiredService<IUserRepository>();
 
         private IAsyncQueryableExecuter QueryableExecuter => LazyServiceProvider.LazyGetRequiredService<IAsyncQueryableExecuter>();
-
-        private IUnitOfWorkManager UnitOfWorkManager => LazyServiceProvider.LazyGetRequiredService<IUnitOfWorkManager>();
 
 
         [UnitOfWork(IsDisabled = true)]
         public async Task<ApiResult<string>> MemberRegistrationAsync(MemberRegistrationDto cmd, CancellationToken token)
         {
 
-            // 创建手动工作单元
+            if (await UserRepository.AnyAsync(x => x.Name == cmd.Name, token)) return ApiResult<string>.Failure(string.Empty, $"用户名【{cmd.Name}】已存在");
+
+            var user = cmd.Adapt<User>();
             using (var uow = UnitOfWorkManager.Begin(true, true))
             {
-                // 插入用户
-                await UserRepo.InsertAsync(cmd.Adapt<User>(), false, cancellationToken: token);
-
-
-                // 提交
-                await uow.SaveChangesAsync(token);
-                await uow.CompleteAsync();
+                try
+                {
+                    await UserRepository.InsertAsync(user, false, cancellationToken: token);
+                    await uow.SaveChangesAsync(token);
+                    await uow.CompleteAsync();
+                }
+                catch (DbUpdateException ex) when (IsDuplicateKeyException(ex))
+                {
+                    return ApiResult<string>.Failure($"用户名【{cmd.Name}】已存在");
+                }
             }
-
-
-
-            //await CurrentUnitOfWork?.SaveChangesAsync(token);
-
             await LocalEventBus.PublishAsync(new MemberRegistrationEto
             {
                 Name = cmd.Name,
                 Password = cmd.Password,
                 Salt = cmd.Salt
             }, false);
-
-            return await Task.FromResult(new ApiResult<string>()
-            {
-                Code = 200,
-                Data = "成功",
-                Message = ""
-            });
+            return ApiResult<string>.Success(user.Id.ToString(), "注册成功!");
+        }
+        private bool IsDuplicateKeyException(DbUpdateException ex)
+        {
+            // SQL Server: 2627 或 2601
+            // MySQL: 1062
+            // PostgreSQL: 23505
+            return ex.InnerException is SqlException sqlEx && (sqlEx.Number == 2627 || sqlEx.Number == 2601);
         }
     }
 }
