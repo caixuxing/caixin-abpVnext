6720f271d038bbf3c923fa827cb5bc2070740c1c
Administrator <11360847@qq.com>
2026-07-07 23:20:09 +0800

    111

---
+++ NiuMa/CaiXin.NiuMa.Domain/Employees/Specifications/CreateEmployeeSpec.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+using Volo.Abp.Specifications;
+
+namespace CaiXin.NiuMa.Domain.Employees.Specifications
+{
+    public class CreateEmployeeSpec : ISpecification<Employee>
+    {
+        public bool IsSatisfiedBy(Employee employee)
+        {
+            if (employee.HireDate.AddYears(1) <= DateTime.Now) throw new ArgumentException("员工入职时间不足一年，无法晋升");
+
+            return true;
+        }
+
+        public Expression<Func<Employee, bool>> ToExpression()
+        {
+            return employee => employee.HireDate.AddYears(1) <= DateTime.Now;
+        }
+    }
+}
