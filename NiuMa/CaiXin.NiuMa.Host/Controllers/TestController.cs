4f7c7431ef91f6e0c3680b81acf32c52f6ba3e56
caxin <11360847@qq.com>
2026-07-22 17:41:32 +0800

    DDD

--- NiuMa/CaiXin.NiuMa.Host/Controllers/TestController.cs
+++ NiuMa/CaiXin.NiuMa.Host/Controllers/TestController.cs
@@ -5,38 +5,30 @@ using CaiXin.NiuMa.Application.Contracts.EmployeeApp.Dto;
 using Microsoft.AspNetCore.Mvc;
 using Volo.Abp.AspNetCore.Mvc;
 
-namespace CaiXin.NiuMa.Host.Controllers
+namespace CaiXin.NiuMa.Host.Controllers;
+
+
+/// <summary>
+/// 测试
+/// </summary>
+[Route("api/[controller]")]
+[ApiController]
+[WrapResult]
+public class TestController(IEmployeeService employeeService) : AbpController, IEmployeeService
 {
-
     /// <summary>
-    /// 测试
+    /// 添加
     /// </summary>
-    [Route("api/[controller]")]
-    [ApiController]
-    [WrapResult]
-    public class TestController(IEmployeeService employeeService) : AbpController, IEmployeeService
-    {
-
-
-        /// <summary>
-        /// 添加
-        /// </summary>
-        /// <param name="cmd"></param>
-        /// <param name="token"></param>
-        /// <returns></returns>
-        [HttpPost("AddEmployee")]
-        public Task<string> Create(CreateEmployeeCmd cmd, CancellationToken token) => employeeService.Create(cmd, token);
-        /// <summary>
-        /// 获取
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        [HttpGet("GetEmployee/{id}")]
-        public Task<EmployeeDto> GetById(Guid id) => employeeService.GetById(id);
-
-
-
-
-
-    }
+    /// <param name="cmd"></param>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    [HttpPost("AddEmployee")]
+    public Task<string> Create(CreateEmployeeCmd cmd, CancellationToken token) => employeeService.Create(cmd, token);
+    /// <summary>
+    /// 获取
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpGet("GetEmployee/{id}")]
+    public Task<EmployeeDto> GetById(Guid id) => employeeService.GetById(id);
 }
